e9f2097a57743f6f0ad92b5ff915fcb1bd81a820
atrgouti <bilalatrgouti2002@gmail.com>
2024-08-15 05:39:12 +0100

    painting the background image

--- src/FlappyDeutsch.java
+++ src/FlappyDeutsch.java
@@ -8,8 +8,32 @@ public class FlappyDeutsch extends JPanel {
     int boardWidth = 360;
     int boardHeight = 640;
 
+    //images
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipImg;
+    Image buttomPipImg;
+
+
     FlappyDeutsch(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
-        setBackground(Color.blue);
+        // setBackground(Color.blue);
+
+        //load images
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        buttomPipImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+
     }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g){
+        //background
+        g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
+    }
+
 }
